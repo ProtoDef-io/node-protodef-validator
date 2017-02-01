8d5320daefd4e93a231608759050aae494767e69
Romain Beaumont <romain.rom1@gmail.com>
2017-02-01 01:12:51 +0100

    everything works

---
+++ README.md
@@ -0,0 +1,49 @@
+# ProtoDef-validator
+
+Validate [ProtoDef](https://github.com/ProtoDef-io/ProtoDef) protocol definition in node
+
+## Installing
+
+```
+npm install protodef-validator
+```
+
+## Usage
+
+See [example](example.js)
+
+## Command Line Interface
+
+You can install this package globally with `npm install -g protodef-validator` and then run `protodef-validator someProtocol.json` to validate it.
+
+## API
+
+### Validator
+
+Class to make validator instances
+
+#### Validator.addType(name,schema)
+
+add the type `name` with schema `schema`
+
+#### Validator.addTypes(schemas)
+
+Add `schemas` which is an object with keys the name of the schemas and values the schema definitions.
+
+#### Validator.validateType(type)
+
+validates a type definition `type`
+
+throws an exception if the type isn't correct
+
+#### Validator.validateProtocol(protocol)
+
+validates a protocol definition `protocol`
+
+throws an exception if the protocol isn't correct
+
+## History
+
+### 1.0.0 (unreleased)
+
+* can validate types and protocols
---
+++ index.js
@@ -0,0 +1,54 @@
+const Ajv = require('ajv');
+const get=require("lodash.get");
+const assert=require("assert");
+
+class Validator {
+  constructor() {
+    this.ajv = new Ajv({verbose:true});
+    this.ajv.addSchema(require("./ProtoDef/schemas/definitions.json"),"definitions");
+    this.ajv.addSchema(require("./ProtoDef/schemas/protocol_schema.json"),"protocol");
+    this.subSchemas = [];
+    this.addDefaultTypes();
+  }
+
+  addDefaultTypes() {
+    this.addType("array",require("./ProtoDef/schemas/array.json"));
+    this.addType("bitfield",require("./ProtoDef/schemas/bitfield.json"));
+    this.addType("buffer",require("./ProtoDef/schemas/buffer.json"));
+    this.addType("container",require("./ProtoDef/schemas/container.json"));
+    this.addType("count",require("./ProtoDef/schemas/count.json"));
+    this.addType("mapper",require("./ProtoDef/schemas/mapper.json"));
+    this.addType("option",require("./ProtoDef/schemas/option.json"));
+    this.addType("pstring",require("./ProtoDef/schemas/pstring.json"));
+    this.addType("switch",require("./ProtoDef/schemas/switch.json"));
+  }
+
+  addTypes(schemas) {
+    Object.keys(schemas).forEach((name) => this.addType(name, schemas[name]));
+  }
+
+  addType(name,schema) {
+    this.subSchemas.push(name);
+    this.ajv.addSchema(schema, name);
+
+    this.ajv.removeSchema("dataType");
+    this.ajv.addSchema({
+      "$schema": "http://json-schema.org/draft-04/schema#",
+      "title": "dataType",
+      "oneOf": [{"$ref": "definitions#/definitions/simpleFieldType"}]
+        .concat(this.subSchemas.map(name => ({"$ref": name})))
+    },"dataType");
+  }
+
+  validateType(type) {
+    let valid = this.ajv.validate("dataType",type);
+    assert.ok(valid, JSON.stringify(this.ajv.errors,null,2));
+  }
+
+  validateProtocol(protocol) {
+    let valid = this.ajv.validate("protocol",protocol);
+    assert.ok(valid, JSON.stringify(this.ajv.errors,null,2));
+  }
+}
+
+module.exports=Validator;
